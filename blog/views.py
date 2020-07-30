e3faa2c7daf6ef1e3f730397600e8b90d33f6349
shuvo14051 <shuvo1137017@gmail.com>
2020-07-30 22:35:03 +0600

    LoginMixin

--- blog/views.py
+++ blog/views.py
@@ -3,36 +3,35 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
-class BlogListView(ListView):
+class BlogListView(LoginRequiredMixin, ListView):
     model = Post
+    login_url = 'login'
     # context_obejct_name = 'posts'
     template_name = 'blog/home.html'
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'blog/detail.html'
 
 
-class BlogCreateView(CreateView): # new
+class BlogCreateView(LoginRequiredMixin, CreateView):  # new
     model = Post
     template_name = 'blog/new.html'
     # fields = ('title','body')
     fields = '__all__'
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'blog/edit.html'
     fields = ['title', 'body']
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'blog/delete.html'
     success_url = reverse_lazy('home')
-
-
